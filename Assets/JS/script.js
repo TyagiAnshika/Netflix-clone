5c4a1931bc0d1afc60e48f8fb95036954e86aaed
Anshika Tyagi <tyagianshika028@gmail.com>
2023-05-23 14:35:52 +0530

    netflix commit

---
+++ Assets/JS/script.js
@@ -0,0 +1,60 @@
+//question 1
+const targetdiv = document.getElementById('answer1');
+const btn= document.getElementById('question1');
+btn.onclick=function(){
+    if(targetdiv.style.display!=="none"){
+        targetdiv.style.display="none";
+    }else{
+    targetdiv.style.display="block";
+    }
+}
+//question 2
+const targetdiv1 = document.getElementById('answer2');
+const btn1= document.getElementById('question2');
+btn1.onclick=function(){
+    if(targetdiv1.style.display!=="none"){
+        targetdiv1.style.display="none";
+    }else{
+    targetdiv1.style.display="block";
+    }
+}
+//question 3
+const targetdiv2 = document.getElementById('answer3');
+const btn2= document.getElementById('question3');
+btn2.onclick=function(){
+    if(targetdiv1.style.display!=="none"){
+        targetdiv2.style.display="none";
+    }else{
+    targetdiv2.style.display="block";
+    }
+}
+//question 4
+const targetdiv3 = document.getElementById('answer4');
+const btn3= document.getElementById('question4');
+btn3.onclick=function(){
+    if(targetdiv3.style.display!=="none"){
+        targetdiv3.style.display="none";
+    }else{
+    targetdiv3.style.display="block";
+    }
+}
+//question 5
+const targetdiv4 = document.getElementById('answer5');
+const btn4= document.getElementById('question5');
+btn4.onclick=function(){
+    if(targetdiv4.style.display!=="none"){
+        targetdiv4.style.display="none";
+    }else{
+    targetdiv4.style.display="block";
+    }
+}
+//question 6
+const targetdiv5 = document.getElementById('answer6');
+const btn5= document.getElementById('question6');
+btn5.onclick=function(){
+    if(targetdiv5.style.display!=="none"){
+        targetdiv5.style.display="none";
+    }else{
+    targetdiv5.style.display="block";
+    }
+}
